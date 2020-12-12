c26c1fbe95243215a587ff27ab214a2b29eeb9a2
Enhex <enhex0@gmail.com>
2020-12-12 19:22:03 +0200

    refactor: usage a single build script

--- build.py
+++ build.py
@@ -1,5 +1,14 @@
 import os
 from sys import platform
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--release", help="Generate release build (debug by default).", action="store_true")
+args = parser.parse_args()
+
+build_type = 'Release' if args.release else 'Debug'
+
+source = os.path.dirname(os.path.realpath(__file__))
 
 def create_symlink(src, dst):
     # create all parent directories of the symlink one
@@ -28,16 +37,21 @@ def build(source, build_type, symlinks = [], symlink_pairs = []):
     # conan
     os.system('conan install "' + source + '/" --build=outdated -s arch=x86_64 -s build_type=' + build_type)
 
-    # choose generator based on OS
-    if platform == 'win32':
-        generator = 'vs2019'
-    else:
-        generator = 'codelite'
-    
-    # premake
+    # choose premake generator based on OS
     os.chdir(source)
-    os.system('premake5 ' + generator + ' --location="' + build_dir + '"')
+
+    def premake_generate(generator):
+        os.system('premake5 ' + generator + ' --location="' + build_dir + '"')
+
+    if platform == 'win32':
+        premake_generate('vs2019')
+    else:
+        premake_generate('gmake2')
+        premake_generate('vscode')
 
     # generate TLS private key and certificate for the test
     os.chdir(build_dir)
     os.system('openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -batch')
+
+
+build(source, build_type)
