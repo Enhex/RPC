7d398661ac27371c40ada7b6edf7d7dd22fed82a
Enhex <enhex0@gmail.com>
2023-12-01 22:40:11 +0200

    upgrade OpenSSL to 3.2.0

--- conanfile.py
+++ conanfile.py
@@ -51,7 +51,7 @@ class RpcConan(ConanFile):
 			if self.options.openssl1:
 				self.requires("openssl/1.1.1w")
 			else:
-				self.requires("openssl/3.1.4")
+				self.requires("openssl/3.2.0")
 
 	# def build(self):
 	# 	run_premake(self)
